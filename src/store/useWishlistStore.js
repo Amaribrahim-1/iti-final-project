8caa090805042784ff8fff5c1ecbd6b5fade5ace
Amaribrahim-1 <amaribrahimforwork1@gmail.com>
2026-09-07 01:45:25 +0300

    feat: persist wishlist with zustand middleware

--- src/store/useWishlistStore.js
+++ src/store/useWishlistStore.js
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 function findWishlistItem(wishlist, id, mediaType) {
   return wishlist.find(
@@ -6,32 +7,40 @@ function findWishlistItem(wishlist, id, mediaType) {
   )
 }
 
-const useWishlistStore = create((set, get) => ({
-  wishlist: [],
+const useWishlistStore = create(
+  persist(
+    (set, get) => ({
+      wishlist: [],
 
-  toggleWishlist: (item, mediaType) => {
-    const isItemExist = get().isInWishlist(item.id, mediaType)
+      toggleWishlist: (item, mediaType) => {
+        const isItemExist = get().isInWishlist(item.id, mediaType)
 
-    if (isItemExist) {
-      get().removeFromWishlist(item.id, mediaType)
-    } else {
-      set({
-        wishlist: [...get().wishlist, { ...item, media_type: mediaType }],
-      })
-    }
-  },
+        if (isItemExist) {
+          get().removeFromWishlist(item.id, mediaType)
+        } else {
+          set({
+            wishlist: [...get().wishlist, { ...item, media_type: mediaType }],
+          })
+        }
+      },
 
-  isInWishlist: (id, mediaType) => {
-    return findWishlistItem(get().wishlist, id, mediaType) !== undefined
-  },
+      isInWishlist: (id, mediaType) => {
+        return findWishlistItem(get().wishlist, id, mediaType) !== undefined
+      },
 
-  removeFromWishlist: (id, mediaType) => {
-    set({
-      wishlist: get().wishlist.filter(
-        (item) => !(item.id === id && item.media_type === mediaType),
-      ),
-    })
-  },
-}))
+      removeFromWishlist: (id, mediaType) => {
+        set({
+          wishlist: get().wishlist.filter(
+            (item) => !(item.id === id && item.media_type === mediaType),
+          ),
+        })
+      },
+    }),
+    {
+      name: 'wishlist',
+      partialize: (state) => ({ wishlist: state.wishlist }),
+    },
+  ),
+)
 
 export default useWishlistStore
